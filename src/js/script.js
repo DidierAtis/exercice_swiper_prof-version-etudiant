4b7ed5d96d6d1a7fdaedc4f686c931e4746e6c0c
DidierAtis <70582678+DidierAtis@users.noreply.github.com>
2020-11-24 09:58:00 -0500

    fini sauf exemple 6

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,55 @@ new Swiper('.swiper-3', {
     autoplay:{
         delay:2000,
     },
-
-
 })
+
+new Swiper('.swiper-4', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
+
+new Swiper('.swiper-5', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        // when window width is >= 420px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 780px
+        780: {
+            slidesPerView: 3,
+            spaceBetween: 30
+
+        },
+        // when window width is >= 1000px
+        1000: {
+            slidesPerView: 4,
+            spaceBetween: 30
+
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 30
+
+        }
+    }
+})
+
+
 
